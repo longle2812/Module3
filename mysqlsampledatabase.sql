617fec8d7b581ec826b20c0ea26694c174164007
longle <lehailong2812@gmail.com>
2021-05-21 14:00:40 +0700

    [Thực hành] Store Procedure

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -8403,3 +8403,34 @@ END //
 DELIMITER ;
 
 call findAllCustomers();
+
+DELIMITER //
+
+CREATE PROCEDURE getCusById
+(IN cusNum INT(11))
+BEGIN
+  SELECT * FROM customers WHERE customerNumber = cusNum;
+END //
+DELIMITER ;
+
+call getCusById(175);
+
+DELIMITER //
+CREATE PROCEDURE SetCounter(
+    INOUT counter INT,
+    IN inc INT
+)
+BEGIN
+    SET counter = counter + inc;
+END//
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1); -- 2
+
+CALL SetCounter(@counter,1); -- 3
+
+CALL SetCounter(@counter,5); -- 8
+
+SELECT @counter; -- 8
